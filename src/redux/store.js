5eb6b15d42b2082db5c23f9818bcc4fbee0ef40d
Gatesvert81 <63714591+Gatesvert81@users.noreply.github.com>
2021-02-26 13:11:08 +0000

    to

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { applyMiddleware } from 'redux'
 import { createStore, combineReducers } from 'redux'
 import logger  from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
-import exampleReducer from './Reducers/reducer';
+import reducer from './Reducers/reducer';
 
 
 
-const store = createStore(exampleReducer, composeWithDevTools(applyMiddleware(logger)) )
+const store = createStore(reducer)
 
 export default store
